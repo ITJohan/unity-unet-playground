5e3cc0a453b7888b82025713c48af98bd1f16813
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-04-20 17:16:25 +0200

    Add 4 different cameras

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    public Camera cam; // Drag camera into here
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // IF I'M THE PLAYER, STOP HERE (DON'T TURN MY OWN CAMERA OFF)
+        if (isLocalPlayer) return;
+ 
+        // DISABLE CAMERA AND CONTROLS HERE (BECAUSE THEY ARE NOT ME)
+        cam.enabled = false;
+        //GetComponent<PlayerControls>().enabled = false;
+        //GetComponent<PlayerMovement>().enabled = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(0f, 10f), 1f, Random.Range(0f, 10f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
