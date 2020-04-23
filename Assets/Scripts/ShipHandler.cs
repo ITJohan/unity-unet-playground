59ea6b5bee999906864dc5d54d8de8e6dc2fc39d
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-04-23 16:41:12 +0200

    Add draw path

--- Assets/Scripts/ShipHandler.cs
+++ Assets/Scripts/ShipHandler.cs
@@ -5,16 +5,110 @@ using UnityEngine.Networking;
 
 public class ShipHandler : NetworkBehaviour
 {
+    // Variables
+    float speed = 5f;
+    int fingerId;
+    List<Vector3> path = new List<Vector3>();
     Rigidbody rb;
-    // Start is called before the first frame update
+    Vector3 currentDirection;
+
+    // Setter for speed
+    public float Speed
+    {
+        set { speed = value; }
+    }
+
+    // Getter and setter for fingerId
+    public int FingerId
+    {
+        get { return fingerId; }
+        set
+        {
+            path.Clear();
+            fingerId = value;
+        }
+    }
+   
+    // Setter for currentDirection
+    public Vector3 CurrentDirection
+    {
+        set { currentDirection = value; }
+    }
+
+    // Adds a point to the path and redraws the path
+    public void AddPointToPath(Vector3 point)
+    {
+        path.Add(point);
+        DrawLines();
+    }
+
+    void DrawLines()
+    {
+        // TODO
+    }
+    
+    // Runs at a fixed framerate
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    // Handle movement based on path exist
+    void HandleMovement()
+    {
+        if (path.Count == 0)
+        {
+            MoveFreely();
+        }
+        else
+        {
+            MoveToPoint();
+        }
+    }
+
+    // Make object go in a straight line after last point
+    void MoveFreely()
+    {
+        rb.isKinematic = false;
+        rb.velocity = currentDirection;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(currentDirection, transform.up), 0.15f);
+    }
+
+    // Move to the next point in path
+    void MoveToPoint()
+    {
+        rb.isKinematic = true;
+
+        // Move one step towards current point
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, path[0], step);
+
+        if (transform.position.Equals(path[0]))
+        {
+            // Delete point if reached
+            path.RemoveAt(0);
+
+            // Recalculate line renderer
+            DrawLines();
+        }
+        else
+        {
+            // Save ccurrent direction
+            currentDirection = (path[0] - transform.position).normalized * speed;
+
+            // Face right direction
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(currentDirection, transform.up), 0.15f);
+        }
+    }
+
+    // Register components on start
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //rb.AddForce(new Vector3(800f, 0f, 600f));
+
+        // TEMPORARY
+        currentDirection = new Vector3(10f, 0, 10f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
 }
+
--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -6,7 +6,71 @@ using UnityEngine.Networking;
 
 public class TouchManager : NetworkBehaviour
 {
+    // Variables
     Dictionary<int, GameObject> fingerIdToShip = new Dictionary<int, GameObject>();
+    Camera cam;
+
+    // Set finger id to -1 on ship and remove from dict
+    void DeregisterShip(Touch currenTouch)
+    {
+        
+    }
+
+    // Set up ship and finger id if touch on ship
+    void InitTouch(Touch currentTouch)
+    {
+        Ray ray = cam.ScreenPointToRay(currentTouch.position);
+        RaycastHit hit;
+
+        // Send out ray and see if ship got touched
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Ship")
+            {
+                // Save ship to dict
+                GameObject shipObject = hit.transform.gameObject;
+                fingerIdToShip.Add(currentTouch.fingerId, shipObject);
+
+                // Set finger id to ship
+                ShipHandler shipHandler = shipObject.GetComponent<ShipHandler>();
+                shipHandler.FingerId = currentTouch.fingerId;
+            }
+        }
+    }
+
+    void RegisterTouchToShip(Touch currentTouch)
+    {
+        int currentFingerId = currentTouch.fingerId;
+        
+        if (fingerIdToShip.ContainsKey(currentFingerId))
+        {
+            GameObject shipObject = fingerIdToShip[currentFingerId];
+
+            // Handle if ship is destroyed while drawing path
+            if (shipObject != null)
+            {
+                ShipHandler shipHandler = shipObject.GetComponent<ShipHandler>();
+                if (currentFingerId == shipHandler.FingerId)
+                {
+                    Vector3 point = cam.ScreenToWorldPoint(new Vector3(currentTouch.position.x, currentTouch.position.y, cam.transform.position.y - shipObject.transform.position.y));
+                    shipHandler.AddPointToPath(point);
+                }
+            }
+        }
+    }
+
+    // Set up camera at start
+    void Start()
+    {
+        // Disable GetMouseButtonDown(0) to register on touch
+        Input.simulateMouseWithTouches = false;
+        cam = GetComponent<Camera>();
+
+        if (isLocalPlayer) return;
+        cam.enabled = false;
+    }
+
+    // Handles all touches on screen each frame
     void Update()
     {
         if (!isLocalPlayer)
@@ -23,37 +87,14 @@ public class TouchManager : NetworkBehaviour
                     case TouchPhase.Began:
                         InitTouch(currentTouch);
                         break;
+                    case TouchPhase.Moved:
+                        RegisterTouchToShip(currentTouch);
+                        break;
+                    case TouchPhase.Ended:
+                        DeregisterShip(currentTouch);
+                        break;
                 }
             }
         }
-    }
-
-    private void InitTouch(Touch currentTouch)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(currentTouch.position);
-        RaycastHit hit;
-        CmdLogOnServer("InitTouch");
-
-        // Send out ray and see if ship got touched
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.collider.tag == "Ship")
-            {
-                // Save ship to dict
-                GameObject shipObject = hit.transform.gameObject;
-                fingerIdToShip.Add(currentTouch.fingerId, shipObject);
-
-                // Set finger id to ship
-                ShipHandler shipHandler = shipObject.GetComponent<ShipHandler>();
-                CmdLogOnServer("Touched ship " + shipObject.name);
-                //shipHandler.SetFingerId(currentTouch.fingerId);
-            }
-        }
-    }
-
-    [Command]
-    void CmdLogOnServer(string msg)
-    {
-        Debug.Log(msg);
     }
 }
